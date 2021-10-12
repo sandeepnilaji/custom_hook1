f2f8b576a9ee0e69a29052c0cf317c404ebc853f
sandeepnilaji <snilaji@gmail.com>
2021-10-12 22:54:01 +0530

    assignment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+function useTimer(sec) {
+  const [time, setTime] = useState();
+  useEffect(() => {
+    let id = setTimeout(() => {
+      setTime(true);
+    }, sec * 1000);
+    return () => clearTimeout(id);
+  });
+
+  return time;
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const ready = useTimer(5);
+  return <div className="App">{ready ? "ready" : "....loading"}</div>;
 }
 
 export default App;
